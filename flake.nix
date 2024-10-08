d1fcec1c4653d4809b7dcf427a170e32b8c4a80a
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2024-10-08 15:07:40 +0200

    :tada: init

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Rust Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; # Use unstable for latest Rust versions
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+          ];
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          name = "rust-dev-env";
+
+          buildInputs = [
+            rust
+          ];
+
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+      }
+    );
+}
