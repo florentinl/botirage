f373147c6c017e558df7bb7e889541f6769cac06
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2024-10-17 16:16:07 +0200

    cross compiling stuff

--- flake.nix
+++ flake.nix
@@ -4,8 +4,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; # Use unstable for latest Rust versions
     flake-utils.url = "github:numtide/flake-utils";
+
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -13,6 +17,7 @@
       nixpkgs,
       flake-utils,
       rust-overlay,
+      crane,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -22,6 +27,17 @@
           inherit system;
           overlays = [ (import rust-overlay) ];
         };
+
+        pkgsCross = import nixpkgs {
+          system = system;
+          crossSystem = "aarch64-linux";
+          overlays = [
+            (import rust-overlay)
+          ];
+        };
+
+        craneLib = (crane.mkLib pkgsCross).overrideToolchain (p: p.rust-bin.stable.latest.default);
+
         rust = pkgs.rust-bin.stable.latest.default.override {
           extensions = [
             "rust-src"
@@ -56,40 +72,30 @@
             pkgs.sqlite
           ];
         };
-      }
-    )
-    // (
-      let
-        pkgsArm = import nixpkgs {
-          system = "aarch64-linux";
-          overlays = [ (import rust-overlay) ];
-        };
-        pkgsCross = import nixpkgs {
-          system = "x86_64-linux";
-          overlays = [
-            (import rust-overlay)
+
+        packages.botirage-aarch64 = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
+
+          nativeBuildInputs = with pkgsCross.pkgsBuildHost; [
+            pkg-config
           ];
-          crossSystem = {
-            config = "aarch64-unknown-linux-gnu";
-          };
-        };
-      in
-      {
-        packages.cross.botirage = pkgsCross.rustPlatform.buildRustPackage {
-          pname = "botirage";
-          version = "0.1.0";
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-          nativeBuildInputs = [
-            pkgsCross.pkg-config
-            pkgsArm.openssl
-            pkgsArm.sqlite
+
+          buildInputs = with pkgsCross.pkgsHostHost; [
+            openssl
+            sqlite
           ];
-          buildInputs = [
-            pkgsArm.openssl
-            pkgsArm.sqlite
-          ];
+
+          CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUNNER = "qemu-aarch64";
+          CARGO_BUILD_TARGET = "aarch64-unknown-linux-gnu";
+          CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER =
+            with pkgsCross.pkgsHostHost;
+            "${stdenv.cc.targetPrefix}cc";
+
+          HOST_CC = with pkgsCross.pkgsBuildHost; "${stdenv.cc.nativePrefix}cc";
+          TARGET_CC = with pkgsCross.pkgsHostHost; "${stdenv.cc.targetPrefix}cc";
         };
+
       }
     );
 }
