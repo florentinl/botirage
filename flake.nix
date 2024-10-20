7305bb47d4309914491099dc75c27f82ab2fd351
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2024-10-20 16:20:43 +0200

    define nixos module to simplify things

--- flake.nix
+++ flake.nix
@@ -9,90 +9,156 @@
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
 
     crane.url = "github:ipetkov/crane";
-    crane.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     {
+      self,
       nixpkgs,
       flake-utils,
       rust-overlay,
       crane,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
-        };
+    (
+      flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+          };
 
-        pkgsCross = import nixpkgs {
-          system = system;
-          crossSystem = "aarch64-linux";
-          overlays = [
-            (import rust-overlay)
-          ];
-        };
+          pkgsCross = import nixpkgs {
+            system = system;
+            crossSystem = "aarch64-linux";
+            overlays = [
+              (import rust-overlay)
+            ];
+          };
 
-        craneLib = (crane.mkLib pkgsCross).overrideToolchain (p: p.rust-bin.stable.latest.default);
+          craneLib = (crane.mkLib pkgs).overrideToolchain (p: p.rust-bin.stable.latest.default);
+          craneLibCross = (crane.mkLib pkgsCross).overrideToolchain (p: p.rust-bin.stable.latest.default);
 
-        rust = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [
-            "rust-src"
-            "rust-analyzer"
-          ];
-        };
-      in
-      {
-        devShell = pkgs.mkShell {
-          name = "rust-dev-env";
+          rustDev = pkgs.rust-bin.stable.latest.default.override {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+            ];
+          };
 
-          buildInputs = [
-            rust
-            pkgs.pkg-config
-            pkgs.sqlite
-          ];
+          mkNixosModuleFromPkg =
+            {
+              pkg,
+            }:
+            {
+              config,
+              lib,
+              ...
+            }:
+            {
+              options.services.botirage = {
+                enable = lib.mkOption {
+                  type = lib.types.bool;
+                  default = false;
+                  description = "Enable the Botirage service";
+                };
 
-          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-        };
+                telegram_api_key = lib.mkOption {
+                  type = lib.types.str;
+                  default = "";
+                  description = "Telegram API key for the bot";
+                };
 
-        defaultPackage = pkgs.rustPlatform.buildRustPackage {
-          pname = "botirage";
-          version = "0.1.0";
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-          nativeBuildInputs = [
-            pkgs.pkg-config
-          ];
-          buildInputs = [
-            pkgs.sqlite
-          ];
-        };
+                database_path = lib.mkOption {
+                  type = lib.types.str;
+                  default = "/etc/botirage/botirage.db";
+                  description = "Path to the database file";
+                };
+              };
 
-        packages.botirage-aarch64 = craneLib.buildPackage {
-          src = craneLib.cleanCargoSource ./.;
-          strictDeps = true;
+              config = lib.mkIf config.services.botirage.enable {
+                systemd.services.botirage = {
+                  enable = true;
+                  description = "CybersecDealerBot";
+                  wantedBy = [ "multi-user.target" ];
+                  environment = {
+                    TELOXIDE_TOKEN = config.services.botirage.telegram_api_key;
+                    DATABASE_PATH = config.services.botirage.database_path;
+                  };
+                  serviceConfig = {
+                    Type = "simple";
+                    ExecStart = lib.getExe pkg;
+                    Restart = "always";
+                  };
+                };
+              };
+            };
+        in
+        {
+          devShell = pkgs.mkShell {
+            name = "rust-dev-env";
 
-          nativeBuildInputs = with pkgsCross.pkgsBuildHost; [
-            pkg-config
-          ];
+            nativeBuildInputs = [
+              pkgs.pkg-config
+            ];
 
-          buildInputs = with pkgsCross.pkgsHostHost; [
-            sqlite
-          ];
+            buildInputs = [
+              rustDev
+              pkgs.sqlite
+            ];
+          };
 
-          CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUNNER = "qemu-aarch64";
-          CARGO_BUILD_TARGET = "aarch64-unknown-linux-gnu";
-          CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER =
-            with pkgsCross.pkgsHostHost;
-            "${stdenv.cc.targetPrefix}cc";
+          defaultPackage = craneLib.buildPackage {
+            src = craneLibCross.cleanCargoSource ./.;
+            strictDeps = true;
 
-          HOST_CC = with pkgsCross.pkgsBuildHost; "${stdenv.cc.nativePrefix}cc";
-          TARGET_CC = with pkgsCross.pkgsHostHost; "${stdenv.cc.targetPrefix}cc";
-        };
+            nativeBuildInputs = with pkgs.pkgsBuildHost; [
+              pkg-config
+            ];
 
+            buildInputs = with pkgs.pkgsHostHost; [
+              sqlite
+            ];
+
+            meta = {
+              mainProgram = "botirage";
+            };
+          };
+
+          packages.botirage-cross-aarch64 = craneLibCross.buildPackage {
+            src = craneLibCross.cleanCargoSource ./.;
+            strictDeps = true;
+
+            nativeBuildInputs = with pkgsCross.pkgsBuildHost; [
+              pkg-config
+            ];
+
+            buildInputs = with pkgsCross.pkgsHostHost; [
+              sqlite
+            ];
+
+            CARGO_BUILD_TARGET = "aarch64-unknown-linux-gnu";
+            CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUNNER = "qemu-aarch64";
+            CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER =
+              with pkgsCross.pkgsHostHost;
+              "${stdenv.cc.targetPrefix}cc";
+
+            HOST_CC = with pkgsCross.pkgsBuildHost; "${stdenv.cc.nativePrefix}cc";
+            TARGET_CC = with pkgsCross.pkgsHostHost; "${stdenv.cc.targetPrefix}cc";
+
+            meta = {
+              mainProgram = "botirage";
+            };
+          };
+
+          nixosModule = mkNixosModuleFromPkg { pkg = self.defaultPackage.${system}; };
+          nixosModuleCrossAarch64 = mkNixosModuleFromPkg {
+            pkg = self.packages.${system}.botirage-cross-aarch64;
+          };
+        }
+      )
+      // {
       }
     );
 }
